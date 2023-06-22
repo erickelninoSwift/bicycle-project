74060e3ffe6ee55d7dd33fddea54dc8184fae7ce
Erick El nino <erick.tshimbombo@yahoo.com>
2023-06-23 01:57:43 +0200

    update

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-const fs= require('fs');
-const fsPromise = require('fs').promises;
+
+
 
 const http = require('http');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+
 const url = require('url');
+const fsPromise = require('fs').promises;
 
 
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(async(req,res) =>{
+  
 
-
-const server = http.createServer((req,res) =>{
-    console.log(req.url);
-
-    const myURl = new URL(req.url,'http://localhost:3000/')
+    const myHost = req.headers.host;
+    const myURl = new URL(req.url,`http://${myHost}/`);
    
     const myPathName = myURl.pathname;
     const id = myURl.searchParams.get('id');
+
+    if(myPathName === '/' && id >= 0 && id <= 5)
+    {
+
+        const html = await fsPromise.readFile(path.join(__dirname,'view','bicycles.html'),'utf-8');
+
+        if(res.statusCode === 200)
+        {
+            res.writeHead(200,{'Content-Type': 'text/html'});
+            res.end(html);
+        }else
+        {
+            console.log('file could not be read');
+        }
+
+    }else if(myPathName === '/bicycle' && id >= 0 && id <= 5)
+    {
+        const html = await fsPromise.readFile(path.join(__dirname,'view','overview.html'),'utf-8');
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.end(html);
+
+    }else 
+    {
+         
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.end('<h1> File not found </h1>');
+    }
      
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    res.end('<h1>Welcome to my Page</h1>')
+   
     
     console.log(myURl);
     console.log(id);
